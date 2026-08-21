9d7c491a81cc7cc0b8643e6351dba6fdf043ae18
Ritesh Karankal <ritesh.karankal200@gmail.com>
2026-08-21 16:36:28 +0530

    feat: implement database connection setup and storage initialization

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,15 +4,37 @@ import (
 	"log"
 
 	"github.com/ritesh-karankal/go-feed/internal/env"
+	"github.com/ritesh-karankal/go-feed/internal/store"
+	"github.com/ritesh-karankal/go-feed/internal/db"
 )
 
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
+
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
+		store: store,
 	}
 
 	mux := app.mount()
