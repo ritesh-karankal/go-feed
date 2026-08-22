35535510331dab9e76afaf9361aa48cb4d3d686e
Ritesh Karankal <ritesh.karankal200@gmail.com>
2026-08-22 11:29:51 +0530

    feat: add get post by ID handler and update post storage methods

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"net/http"
-
+	"errors"
+	"strconv"
+	"github.com/go-chi/chi/v5"
 	"github.com/ritesh-karankal/go-feed/internal/store"
 )
 
 type CreatePostPayload struct {
-	Title string `json:"title"`
-	Content string `json:"content"`
-	Tags []string `json:"tags"`
+	Title   string   `json:"title"`
+	Content string   `json:"content"`
+	Tags    []string `json:"tags"`
 }
 
 func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request) {
@@ -20,9 +22,9 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 	}
 
 	post := &store.Post{
-		Title: payload.Title,
+		Title:   payload.Title,
 		Content: payload.Content,
-		Tags: payload.Tags,
+		Tags:    payload.Tags,
 		// TODO: change after auth
 		UserID: 1,
 	}
@@ -31,6 +33,35 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 
 	if err := app.store.Posts.Create(ctx, post); err != nil {
 		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	if err := writeJSON(w, http.StatusCreated, post); err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+}
+
+
+func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
+	idParam := chi.URLParam(r, "postID")
+	id, err := strconv.ParseInt(idParam, 10, 64)
+	if err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	ctx := r.Context()
+
+	post, err := app.store.Posts.GetByID(ctx, id)
+	if err != nil {
+		switch {
+		case errors.Is(err, store.ErrNotFound):
+			writeJSONError(w, http.StatusNotFound, err.Error())
+		default:
+			writeJSONError(w, http.StatusInternalServerError, err.Error())
+		}
+
 		return
 	}
 
--- internal/store/posts.go
+++ internal/store/posts.go
@@ -2,6 +2,7 @@ package store
 
 import (
 	"context"
+	"errors"
 	"database/sql"
 	"github.com/lib/pq"
 )
@@ -43,4 +44,34 @@ func (s *PostStore) Create(ctx context.Context, post *Post) error {
 	}
 
 	return nil
+}
+
+func (s *PostStore) GetByID(ctx context.Context, id int64) (*Post, error) {
+	query := `
+		SELECT id, user_id, title, content, tags, created_at, updated_at
+		FROM posts
+		WHERE id = $1
+	`
+
+	var post Post
+	err := s.db.QueryRowContext(ctx, query, id).Scan(
+		&post.ID,
+		&post.UserID,
+		&post.Title,
+		&post.Content,
+		pq.Array(&post.Tags),
+		&post.CreatedAt,
+		&post.UpdatedAt,
+	)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	return &post, nil
 }
