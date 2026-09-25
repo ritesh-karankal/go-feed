bc4873a2a3fca4ce436d866e74133a7be3f632cc
Ritesh Karankal <ritesh.karankal200@gmail.com>
2026-09-25 14:19:48 +0530

    feat: Update getUserFeedHandler to fetch user feed based on authenticated user context

--- cmd/api/feed.go
+++ cmd/api/feed.go
@@ -48,7 +48,9 @@ func (app *application) getUserFeedHandler(w http.ResponseWriter, r *http.Reques
 
 	ctx := r.Context()
 
-	feed, err := app.store.Posts.GetUserFeed(ctx, int64(2), fq)
+	user := getUserFromContext(r)
+
+	feed, err := app.store.Posts.GetUserFeed(ctx, user.ID, fq)
 	if err != nil {
 		app.internalServerError(w, r, err)
 		return
